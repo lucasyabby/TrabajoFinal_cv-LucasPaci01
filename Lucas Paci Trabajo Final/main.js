ad445b1e3067f966b25d7e2cd7ce7f10cfdfa3de
lucasyabby <61995785+lucasyabby@users.noreply.github.com>
2023-02-01 16:18:35 -0300

    Add files via upload

---
+++ Lucas Paci Trabajo Final/main.js
@@ -0,0 +1,7 @@
+function sumar(){
+    var numero1 = parseInt(document.getElementById("num1").value);
+    var numero2 = parseInt(document.getElementById("num2").value);
+    var resultado = num1 + numero2;
+
+    alert(numero1 + numero2);
+}
